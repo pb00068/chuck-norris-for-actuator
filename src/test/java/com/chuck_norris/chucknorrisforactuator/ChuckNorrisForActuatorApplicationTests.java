7fd2711ffaa1c6417dcfa619337ecf239c12b1dd
Demetz <Guenther.Demetz@wuerth-phoenix.com>
2021-01-22 15:09:42 +0100

    Initial commit

---
+++ src/test/java/com/chuck_norris/chucknorrisforactuator/ChuckNorrisForActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuck_norris.chucknorrisforactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisForActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
